3cb2515a17c9b7d2b91260cbf32de91080c333fa
ASLawan <nawalnitsua@gmail.com>
2025-02-21 22:15:55 +0100

    Update: swagger API documentation url

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -26,5 +26,7 @@ const swaggerSpec = swaggerJSDoc(swaggerOptions);
 // Function to serve Swagger docs
 export const setupSwagger = (app: Application) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("📄 Swagger Docs available at: http://localhost:5000/api-docs");
+  console.log(
+    "📄 Swagger Docs available at: https://transaction-tracker-gkfw.onrender.com/api-docs"
+  );
 };
