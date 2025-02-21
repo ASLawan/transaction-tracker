b0c3db07fc57d0dba04deaa5ca50baec680531ee
ASLawan <nawalnitsua@gmail.com>
2025-02-21 10:08:09 +0100

    Fix cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,13 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "localhost";
 
 // Middlware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://transaction-tracker-static.onrender.com", // Allow only your frontend domain
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  })
+);
 app.use(express.json());
 
 //Setup Swagger
