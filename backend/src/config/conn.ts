7b754effc2a2a6aa17e23ed43661466587761897
ASLawan <nawalnitsua@gmail.com>
2025-02-21 03:40:12 +0100

    Update: modify postgres connection str

--- backend/src/config/conn.ts
+++ backend/src/config/conn.ts
@@ -35,7 +35,7 @@ dotenv.config();
 // Use DATABASE_URL if available (for Render deployment)
 const databaseUrl =
   process.env.DATABASE_URL ||
-  "postgresql://super_admin:O585oT4dHckChGT9X3JWyGplpWuEn2pK@dpg-curu93l6l47c73a34qc0-a:5432/transactions_b0fn";
+  "postgresql://super_admin:O585oT4dHckChGT9X3JWyGplpWuEn2pK@dpg-curu93l6l47c73a34qc0-a/transactions_b0fn";
 
 export const sequelize = databaseUrl
   ? new Sequelize(databaseUrl, {
